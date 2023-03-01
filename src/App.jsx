84df60e2a059b7ad9e5a593f98b069c3c2063c32
BrundhaBH <gowdabrundha2000@gmail.com>
2023-03-01 21:44:12 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const App= ()=>{
     return(
-    <div>App</div>
+    <>
+    <div> App </div>
+    <h1> HAI </h1>
+    <p> 5+5 </p>
+    <h2> {2+2} </h2>
+    </>
 )
     }
 
