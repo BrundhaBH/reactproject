902d76c26a518fa80d30830ba765ca4598e72ae9
BrundhaBH <gowdabrundha2000@gmail.com>
2023-03-03 11:57:11 +0530

    Done

---
+++ src/Component/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Logo from './Logo';
+import Menu from './Menu';
+
+const Nav=()=>{
+    return(
+        <>
+        <Logo/>
+        <Menu/>
+        </>
+    )
+}
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-import App from './App';
+import Nav from './Component/Nav';
+import Logo from './Component/Logo';
+import Menu from './Component/Menu';
+import App from './Component/App';
 
+createRoot(document.getElementById('root')).render(<Nav/>)
+createRoot(document.getElementById('root')).render(<Logo/>)
+createRoot(document.getElementById('root')).render(<Menu/>)
 createRoot(document.getElementById('root')).render(<App/>)
