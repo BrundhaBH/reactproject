d3ffd2e82566a0c906070492478729fb060d1683
BrundhaBH <gowdabrundha2000@gmail.com>
2023-03-16 21:27:35 +0530

    Push

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-//import Institute from './Props/Institute';
-import Movie from './States/Movie';
+import Institute from './Props/Institute';
+//import Movie from './States/Movie';
 
-//createRoot(document.getElementById('root')).render(<Institute/>)
+createRoot(document.getElementById('root')).render(<Institute/>)
 
-createRoot(document.getElementById('root')).render(<Movie/>)
+//createRoot(document.getElementById('root')).render(<Movie/>)
